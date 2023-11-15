c0fe2954be9bd5af789896045d21c6a12f8c8933
Vihan Perera <vihanpereraux@gmail.com>
2023-11-15 15:26:04 +0000

    Add muse-python packages

--- assets/js/hydra.js
+++ assets/js/hydra.js
@@ -51,22 +51,11 @@ function getLocalStream() {
 
 // animation
 osc(10, 0.5, 0.001)
-    .kaleid(10)
-    .color(1, .5, 0.2)
-    .colorama(() => envVolume * 0.005)
-    .rotate(10)
+    .kaleid(() => envVolume * 2)
+    // add EEG data here
+    .color(.5, .5, 0.2)
+    .colorama(() => envVolume * .01)
+    .rotate(() => mouseXCord * 0.001)
     .modulateRotate(o0, () => envVolume, 0.001)
     .modulate(o0, 1)
 .out(o0)
-
-
-
-// osc(5, 0.5, 0.001)
-// .kaleid([3,4,5,7,8,9,10].fast(0.1))
-// // .color(0.5, 0.3)
-// // .colorama(0.4)
-// // .rotate(0.009,()=>Math.sin(time)* -0.001 )
-// // .modulateRotate(o0,()=>Math.sin(time) * 0.003)
-// // .modulate(o0, 0.9)
-// // .scale(0.9)
-// .out(o0)
